10e32ee97bb8c2e42de6ce171566fea18471a051
bbruno5 <bbruno5.ever@gmail.com>
2018-07-10 18:43:08 -0300

    Minor improvements

--- src/main/java/com/b5team/postrequest/Settings.java
+++ src/main/java/com/b5team/postrequest/Settings.java
@@ -2,30 +2,30 @@ package com.b5team.postrequest;
 
 import java.net.URL;
 
-public class Settings {
+class Settings {
 	
 	private URL url;
 	private String protocol;
 	private String pwd;
 	
-	public String getUrl() {
+	String getUrl() {
 		return url.toString();
 	}
 	
-	public String getProtocol() {
+	String getProtocol() {
 		return protocol;
 	}
 	
-	public String getPwd() {
+	String getPwd() {
 		return pwd;
 	}
 	
-	public void setUrl(URL url, String protocol) {
+	void setUrl(URL url, String protocol) {
 		this.url = url;
 		this.protocol = protocol;
 	}
 	
-	public void setPwd(String pwd) {
+	void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
 }
